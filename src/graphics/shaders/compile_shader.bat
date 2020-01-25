13cd769d6c8578a650d2cbaee5b7466cbfd66b01
David <davidgonzalezmartin94@gmail.com>
2020-01-25 14:01:46 +0100

    Shader compilation from source

--- src/graphics/shaders/compile_shader.bat
+++
@@ -1,5 +0,0 @@
-glslangValidator.exe triangle.vert -V -o triangle.vert.spv
-glslangValidator.exe triangle.frag -V -o triangle.frag.spv
-glslangValidator.exe model.vert -V -o model.vert.spv
-glslangValidator.exe model.vert -V -o model.frag.spv
-glslangValidator.exe model.mesh -V -o model.mesh.spv
--- src/tripleR.cpp
+++ src/tripleR.cpp
@@ -5,6 +5,45 @@
 #include "os/win32/red_win32.h"
 #include "graphics/red_graphics.h"
 
+void compileShaders()
+{
+    char shaders[MAX_FILENAME_LENGTH][5];
+
+    const char* shaderNames[] =
+    {
+        "triangle.vert",
+        "triangle.frag",
+        "model.vert",
+        "model.frag",
+        "model.mesh",
+    };
+
+    const char* shaderOutputNames[] =
+    {
+        "triangle.vert.spv",
+        "triangle.frag.spv",
+        "model.vert.spv",
+        "model.frag.spv",
+        "model.mesh.spv",
+    };
+
+    const char* commandName = "glslangValidator %s -V -o %s";
+
+    char buffer[MAX_FILENAME_LENGTH];
+    char bufferOutput[MAX_FILENAME_LENGTH];
+
+    for (u32 i = 0; i < ARRAYCOUNT(shaderNames); i++)
+    {
+        char out[MAX_FILENAME_LENGTH];
+        os_strcpy(buffer, shaderDirectory);
+        os_strcpy(bufferOutput, buffer);
+        os_appendFileOrDirectory(buffer, shaderNames[i]);
+        os_appendFileOrDirectory(bufferOutput, shaderOutputNames[i]);
+        os_sprintf(out, commandName, buffer, bufferOutput);
+        system(out);
+    }
+}
+
 #if RRR_WIN32
 WINMAIN_HEADER
 #else
@@ -18,6 +57,9 @@ int main(int argc, const char* argv[])
     os_fillCurrentWorkingDirectory();
     os_fillShadersDirectory();
     os_fillAssetsDirectory();
+#if RRR_WIN32
+    compileShaders();
+#endif
 
     vulkan_renderer vk;
     vk_load(&vk, &handles, &windowDimensions);
